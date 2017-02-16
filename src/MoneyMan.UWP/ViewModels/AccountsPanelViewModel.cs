2c8480c3220ed73de7ae40e145b68880cccccb55
Andrew Arnott <andrewarnott@gmail.com>
2017-02-16 08:35:08 -0800

    State of the solution when VS crashed

---
+++ src/MoneyMan.UWP/ViewModels/AccountsPanelViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyMan.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Nerdbank.MoneyManagement.ViewModels;
+
+    public class AccountsPanelViewModel : BindableBase
+    {
+        public ObservableCollection<AccountViewModel> Accounts { get; set; }
+    }
+}
